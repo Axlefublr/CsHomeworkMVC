b8b015cf4de767ef203db27f55aa1549190bbb7e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-14 23:32:50 +0800

    add blogging functionality

--- MvcStartApp/Controllers/HomeController.cs
+++ MvcStartApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -9,29 +10,51 @@ using MvcStartApp.Models;
 
 namespace MvcStartApp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly IBlogRepository _repo;
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, IBlogRepository repo)
+		{
+			_logger = logger;
+			_repo = repo;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			User newUser = new()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = "Andrey",
+				LastName = "Petrov",
+				JoinDate = DateTime.Now
+			};
+			await _repo.AddUser(newUser);
+			Console.WriteLine($"User with id {newUser.Id}, named {newUser.FirstName} was successfully added on {newUser.JoinDate}");
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Authors()
+		{
+			var authors = await _repo.GetUsers();
+			Console.WriteLine("See all blog authors:");
+			foreach (var author in authors)
+			{
+				Console.WriteLine($"Author name {author.FirstName}, joined {author.JoinDate}");
+			}
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
---
+++ MvcStartApp/Models/Db/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcStartApp;
+
+namespace MvcStartApp
+{
+	public class BlogRepository : IBlogRepository
+	{
+		private readonly BlogContext _context;
+		public BlogRepository(BlogContext context)
+		{
+			_context = context;
+		}
+		public async Task AddUser(User user)
+		{
+			var entry = _context.Entry(user);
+			if (entry.State == EntityState.Detached)
+			{
+				await _context.Users.AddAsync(user);
+			}
+			await _context.SaveChangesAsync();
+		}
+		
+		public async Task<User[]> GetUsers() {
+			return await _context.Users.ToArrayAsync();
+		}
+	}
+}
